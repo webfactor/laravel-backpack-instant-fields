1c17616c17471b59bb3a04ac42f60f1a7075e9c5
Thomas Swonke <thomas.swonke@webfactor.de>
2018-05-16 00:46:57 +0200

    [EH] add modal views

---
+++ resources/views/modal/create.blade.php
@@ -0,0 +1,56 @@
+@extends('webfactor::modal.modal_layout')
+
+@section('header')
+    <h3 class="box-title">{{ trans('backpack::crud.add_a_new') }} {{ $crud->entity_name }}</h3>
+@endsection
+
+@section('content')
+    <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+        @include('crud::inc.grouped_errors')
+
+        <!-- load the view from the application if it exists, otherwise load the one in the package -->
+            @if(view()->exists('vendor.backpack.crud.form_content'))
+                @include('vendor.backpack.crud.form_content', ['fields' => $crud->getFields('create')])
+            @else
+                @include('crud::form_content', ['fields' => $crud->getFields('create')])
+            @endif
+        </div>
+    </div>
+@endsection
+
+@section('footer')
+    @include('webfactor::modal.inc.form_save_buttons')
+@endsection
+
+@push('crud_fields_scripts')
+    <script>
+        $("#create_{{ $entity }}").submit(function (e) {
+
+            $.ajax({
+                type: "POST",
+                url: "/{{ $crud->route . '/ajax' }}",
+                data: $("#create_{{ $entity }}").serialize(), // serializes the form's elements.
+                success: function (data) {
+                    new PNotify({
+                        type: "success",
+                        title: "Erfolg",
+                        text: "gespeichert"
+                    });
+
+                    $("#{{ $entity }}_modal").modal('toggle');
+                },
+                error: function (data) {
+                    new PNotify({
+                        type: "error",
+                        title: "Fehler",
+                        text: "Nicht gespeichert: " + data.responseText
+                    });
+                }
+            });
+
+            e.preventDefault(); // avoid to execute the actual submit of the form.
+        });
+    </script>
+
+@endpush
