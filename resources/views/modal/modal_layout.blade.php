413142af96618a2395f2afedd2f840534ea407ce
Thomas Swonke <thomas.swonke@webfactor.de>
2018-07-16 00:13:10 +0200

    reformat

--- resources/views/modal/modal_layout.blade.php
+++ resources/views/modal/modal_layout.blade.php
@@ -2,18 +2,18 @@
 <form method="post" id="{{ 'create_'.$entity }}" accept-charset="UTF-8">
     {{--'files'=>$crud->hasUploadFields('create')])--}}
     {{ csrf_field() }}
-<div class="modal-header">
-    <button type="button" class="close" data-toggle="modal" data-target="#{{ $entity }}_modal" aria-label="Close">
-        <span aria-hidden="true">&times;</span>
-    </button>
-    @yield('header')
-</div>
-<div class="modal-body" id="modal-body">
-    @yield('content')
-</div>
+    <div class="modal-header">
+        <button type="button" class="close" data-toggle="modal" data-target="#{{ $entity }}_modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
+        @yield('header')
+    </div>
+    <div class="modal-body" id="modal-body">
+        @yield('content')
+    </div>
 
-<div class="modal-footer">
-    @yield('footer')
-</div>
+    <div class="modal-footer">
+        @yield('footer')
+    </div>
 </form>
 @stack('crud_fields_scripts')
