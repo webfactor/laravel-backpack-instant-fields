78cd73cdd667447cbc45e88b4323d8f1bb906142
Thomas Swonke <thomas.swonke@webfactor.de>
2018-07-16 00:12:16 +0200

    remove unused statements

--- src/InstantFieldsServiceProvider.php
+++ src/InstantFieldsServiceProvider.php
@@ -3,10 +3,6 @@
 namespace Webfactor\Laravel\Backpack\InstantFields;
 
 use Illuminate\Support\ServiceProvider;
-use Webfactor\Laravel\Generators\Commands\MakeBackpackCrudController;
-use Webfactor\Laravel\Generators\Commands\MakeBackpackCrudModel;
-use Webfactor\Laravel\Generators\Commands\MakeBackpackCrudRequest;
-use Webfactor\Laravel\Generators\Commands\MakeEntity;
 
 class InstantFieldsServiceProvider extends ServiceProvider
 {
