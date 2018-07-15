ed600879a8a9dddf0ef1247c560863a841f48447
Thomas Swonke <thomas.swonke@webfactor.de>
2018-07-16 00:12:48 +0200

    extract button blade

--- resources/views/fields/select2_from_ajax.blade.php
+++ resources/views/fields/select2_from_ajax.blade.php
@@ -37,23 +37,7 @@
     </select>
 
     @if (isset($field['on_the_fly']))
-        <button href="{{ $field['on_the_fly']['create_view'] ?? backpack_url($field['on_the_fly']['entity']).'/ajax/create' }}"
-                type="button"
-                class="btn btn-primary"
-                data-toggle="modal"
-                data-target="#{{ $field['on_the_fly']['entity'] ?? 'ajax_entity' }}_modal">
-            {{ trans('backpack::crud.add') }}
-        </button>
-        <div class="modal fade"
-             id="{{ $field['on_the_fly']['entity'] ?? 'ajax_entity' }}_modal"
-             tabindex="-1"
-             role="dialog"
-             aria-labelledby="{{ $field['on_the_fly']['entity'] ?? 'ajax_entity' }}_modal"
-             aria-hidden="true">
-            <div class="modal-dialog" role="document">
-                <div class="modal-content"></div>
-            </div>
-        </div>
+        @include('webfactor::fields.inc.button-add')
     @endif
 
     {{-- HINT --}}
