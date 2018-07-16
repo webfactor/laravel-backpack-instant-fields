e80afce390e0d920e81570152440e96d70ee1eda
Thomas Swonke <thomas.swonke@webfactor.de>
2018-07-16 22:50:15 +0200

    move modal code to fake button

--- resources/views/buttons/create.blade.php
+++ resources/views/buttons/create.blade.php
@@ -6,13 +6,3 @@
     data-target="#{{ $crud->instantCreateButton['name'] }}_modal">
     {!! $crud->instantCreateButton['content'] !!}
 </a>
-<div class="modal fade"
-     id="{{ $crud->instantCreateButton['name'] }}_modal"
-     tabindex="-1"
-     role="dialog"
-     aria-labelledby="{{ $crud->instantCreateButton['name'] }}_modal"
-     aria-hidden="true">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content"></div>
-    </div>
-</div>
---
+++ resources/views/buttons/fake.blade.php
@@ -0,0 +1,10 @@
+<div class="modal fade"
+     id="{{ $crud->instantCreateButton['name'] }}_modal"
+     tabindex="-1"
+     role="dialog"
+     aria-labelledby="{{ $crud->instantCreateButton['name'] }}_modal"
+     aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content"></div>
+    </div>
+</div>
