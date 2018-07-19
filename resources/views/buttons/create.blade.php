578817ac2065420d2355e636f4c3bf0a8f5cbbae
Thomas Swonke <thomas.swonke@webfactor.de>
2018-07-19 14:12:18 +0200

    fix so that one can use multiple create fields in the list view

--- resources/views/buttons/create.blade.php
+++ resources/views/buttons/create.blade.php
@@ -1,8 +1,10 @@
-<a
-    href="{{ backpack_url($crud->instantCreateButton['name'] . '/ajax/create?' . $crud->instantCreateButton['entity'] . '=' . $entry->id) }}"
-    type="button"
-    class="{{ $crud->instantCreateButton['class'] }}"
-    data-toggle="modal"
-    data-target="#{{ $crud->instantCreateButton['name'] }}_modal">
-    {!! $crud->instantCreateButton['content'] !!}
-</a>
+@isset($crud->instantCreateButtons[$button->name])
+    <a
+        href="{{ backpack_url($crud->instantCreateButtons[$button->name]['name'] . '/ajax/create?' . $crud->instantCreateButtons[$button->name]['entity'] . '=' . $entry->id) }}"
+        type="button"
+        class="{{ $crud->instantCreateButtons[$button->name]['class'] }}"
+        data-toggle="modal"
+        data-target="#{{ $crud->instantCreateButtons[$button->name]['name'] }}_modal">
+        {!! $crud->instantCreateButtons[$button->name]['content'] !!}
+    </a>
+@endisset
